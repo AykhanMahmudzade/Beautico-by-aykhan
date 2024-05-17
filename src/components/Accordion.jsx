0e7e09d3aca9e47c34b5b9c87a4b286a6e325b98
AykhanMahmudzade <ayxan.mahmudzade@mail.ru>
2024-05-18 02:09:48 +0400

    coorect

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -56,7 +56,7 @@ const Accordion = () => {
 
 
 
-<div className="acrd pt-4">
+<div className="acrd pt-4 ">
                     <div class="accordion-item">
                       <h2 class="accordion-header">
                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
